552b4e886bc4ec411bb3fbb6d482fb9a43501618
Udaya Ranga <udaya.ranga@arm.com>
2016-01-07 15:37:53 +0000

    Coursera: W2: Started Gradient Descent
    Change-Id: I123528d5602c4b8fa34e777389d16c0c20f35d4d

--- coursera/machine-learning-ex1/ex1/gradientDescent.m
+++ coursera/machine-learning-ex1/ex1/gradientDescent.m
@@ -6,8 +6,10 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
+diff = 0.0
 
-for iter = 1:num_iters
+for iter = 1:2
+%for iter = 1:1
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
@@ -16,7 +18,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
+    diff = ((X.*(X*theta - y)).*alpha./m);
+    theta = theta - [sum(diff(:, 1)); sum(diff(:, 2))];
+    fprintf('theta: %f\n', theta);
 
 
 
@@ -27,7 +31,9 @@ for iter = 1:num_iters
 
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
+    fprintf('J_history: %f\n', J_history);
 
 end
+%fprintf('size of diff: %d %d', size(diff, 1), size(diff, 2));
 
 end
