abbd747bab0f2a05235e7684cfc0e51e20ba5e92
Udaya Ranga <uday87j@gmail.com>
2016-02-18 21:47:49 +0000

    image_segment: run k-means on greyscale 2D

--- image_segment/image_segment.m
+++ image_segment/image_segment.m
@@ -1,4 +1,8 @@
 function [segments] = image_segment(image_name, num_segments)
+% Create segments in an image based on pixel values
+
+K = num_segments;
+cluster_colours = [0:64/K:63];  % Based on default colourmap()
 
 [img, map, alpha] = imread(image_name);
 
@@ -7,15 +11,33 @@ img = rgb2gray(img);    % Try RGB later
 width = size(img, 1);
 height = size(img, 2);
 
-X = img(:)';    % Get a row vector of all pixels in image
+X = img(:);    % Get a row vector of all pixels in image
 
-initial_centroids = kMeansInitCentroids(X, num_segments);
+[idx] = find_min_kmeans(X, 20, 30, 2);
 
-[centroids, idx] = runkMeans(X, initial_centroids, 10, false);
+%initial_centroids = kMeansInitCentroids(X, num_segments);
+%[centroids, idx] = runkMeans(X, initial_centroids, 10, false);
+%disp(size(idx));
 
-rec_img = reshape(idx, width, height);
+rec_img = zeros(size(idx));
 
-%imshow(rec_img);
+% Colour code rec_img based on idx val
+for i = 1:size(idx, 1)
+    rec_img(i) = cluster_colours(idx(i));
+end
+
+rec_img = reshape(rec_img, width, height);
+%save -ascii "segments.txt" rec_img
+
+figure(1);
+imshow(img);
+%save -ascii "image.txt" img
+
+figure(2);
+image(rec_img);
+
+disp("\nDone plotting images");
+
 segments = rec_img;
 
 end
--- image_segment/main.m
+++ image_segment/main.m
@@ -1,7 +1,8 @@
 % Main file that runs the image clustering algorithm
 
 seg = image_segment("test/cat1.jpeg", 2);
+%img = imresize("test/pancake.jpg", 100, 100);
 
-imshow(seg);
+%imshow(seg);
 
 pause;
