54dc7cdb91711eb2674944a8adf9edfe8a3aa290
Udaya Ranga <udaya.ranga@arm.com>
2016-02-19 17:02:56 +0000

    image_segment: Try plotting rgb for clusters
    Change-Id: I40da5b1462a12d9909b0ed7817a520c824379f01

--- image_segment/image_segment.m
+++ image_segment/image_segment.m
@@ -8,18 +8,18 @@ cluster_colours = [0:64/K:63];  % Based on default colourmap()
 
 %img = rgb2gray(img);    % Try RGB later
 
-[iimg, cmap] = rgb2ind(img);
-save -ascii "ind.txt" iimg;
+[ind_img, cmap] = rgb2ind(img);
+save -ascii "ind.txt" ind_img;
 
-width = size(iimg, 1);
-height = size(iimg, 2);
+width = size(ind_img, 1);
+height = size(ind_img, 2);
 
-pix = iimg(:);
+pix = ind_img(:);
 X = zeros(size(pix, 1), 3); % 3 for RGB
 
-disp(size(pix));
-disp(size(X));
-disp(size(cmap));
+% disp(size(pix));
+% disp(size(X));
+% disp(size(cmap));
 
 for i = 1:size(pix, 1)
     %disp(i);
@@ -27,7 +27,9 @@ for i = 1:size(pix, 1)
     X(i, :) = cmap(pix(i) + 1, :);  % cmap indexing start from 0
 end
 
-%[idx] = find_min_kmeans(X, 20, 30, 2);
+save -ascii "X.txt" X;
+
+%[idx] = find_min_kmeans(X, 20, 30, 2); $TODO: Enable this later if necesasry
 
 initial_centroids = kMeansInitCentroids(X, num_segments);
 [centroids, idx] = runkMeans(X, initial_centroids, 10, false);
@@ -49,6 +51,19 @@ imshow(img);
 
 figure(2);
 image(rec_img);
+
+% TODO: Display figure 3 with mean pixel RGB of categories
+mean_k_pixels = zeros(K, 3);
+for i = 1:K
+    Xk = get_category(X, idx, i);
+    disp(size(Xk));
+    mean_k_pixels(i, :) = (sum(Xk, 1) / size(Xk, 1));
+    set_category(X, idx, mean_k_pixels(i), i);
+end
+
+figure(3);
+image(X);
+%imshow(X);
 
 disp("\nDone plotting images");
 
